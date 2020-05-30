3c6b2f7e53328f8cf2cbf5eff66974774bb6f547
Seth Purr <eric.purrington@gmail.com>
2020-05-30 09:21:34 -0700

    connection and .env files updated

---
+++ config/connection.js
@@ -0,0 +1,16 @@
+var mysql = require("mysql");
+require("dotenv").config();
+
+var connection = mysql.createConnection({
+    host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: "burger_db",
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+});
+
+module.exports = connection;
