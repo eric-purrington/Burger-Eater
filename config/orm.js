f92f6b44e47c6b8064a6a347b23c3162b76d48e1
Seth Purr <eric.purrington@gmail.com>
2020-05-30 16:59:22 -0700

    orm functions written

--- config/orm.js
+++ config/orm.js
@@ -1,22 +1,22 @@
 var connection = require("./connection.js");
 
 var orm = {
-    selectAll: function (, cb) {
-        var queryString = "SELECT * FROM burgers_db";
+    selectAll: function (selection, tableName, cb) {
+        var queryString = `SELECT ${selection} FROM ${tableName}`;
         connection.query(queryString, function (err, result) {
             if (err) throw err;
             cb(result);
         });
     },
-    insertOne: function (, cb) {
-        var queryString = "";
+    insertOne: function (tableName, colName, valueToInsert, cb) {
+        var queryString = `INSERT INTO ${tableName} (${colName}) VALUES (${valueToInsert})`;
         connection.query(queryString, function (err, result) {
             if (err) throw err;
             cb(result);
         });
     },
-    updateOne: function (, cb) {
-        var queryString = "";
+    updateOne: function (tableName, colName1, updatedValue1, colName2, updatedValue2, id, cb) {
+        var queryString = `UPDATE ${tableName} SET ${colName1} = ${updatedValue1}, ${colName2} = ${updatedValue2} WHERE id = ${id}`;
         connection.query(queryString, function (err, result) {
             if (err) throw err;
             cb(result);
