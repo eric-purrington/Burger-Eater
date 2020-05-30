298504e65035082098b6b5949554fd1834bc1349
Seth Purr <eric.purrington@gmail.com>
2020-05-30 15:49:34 -0700

    ground work for orm.js

---
+++ config/orm.js
@@ -0,0 +1,27 @@
+var connection = require("./connection.js");
+
+var orm = {
+    selectAll: function (, cb) {
+        var queryString = "";
+        connection.query(queryString, function (err, result) {
+            if (err) throw err;
+            cb(result);
+        });
+    },
+    insertOne: function (, cb) {
+        var queryString = "";
+        connection.query(queryString, function (err, result) {
+            if (err) throw err;
+            cb(result);
+        });
+    },
+    updateOne: function (, cb) {
+        var queryString = "";
+        connection.query(queryString, function (err, result) {
+            if (err) throw err;
+            cb(result);
+        });
+    }
+}
+
+module.exports = orm;
