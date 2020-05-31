6c1eb890ba42c56c8d72a4310b21e51e32412e76
Seth Purr <eric.purrington@gmail.com>
2020-05-30 17:32:01 -0700

    controller functions written, double check devoured variables

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,4 +3,25 @@ var router = express.Router();
 
 var burger = require("../models/burger.js");
 
-router.get()
+router.get("/", function (req, res) {
+    burger.selectAll(function (data) {
+        res.render("index", { burgers: data });
+    });
+});
+
+router.post("/api/burgers", function (req, res) {
+    burger.insertOne("burger_name", req.body.burger_name, function (result) {
+        res.json({ id: result.insertId });
+    });
+});
+
+router.put("/api/burgers/:id", function (req, res) {
+    burger.updateOne("burger_name", req.body.burger_name, "devoured", req.body.devoured, req.params.id, function (result) {
+        if (result.changedRows === 0) {
+            return res.status(404).end();
+        }
+        res.status(200).end();
+    });
+});
+
+module.exports = router;
