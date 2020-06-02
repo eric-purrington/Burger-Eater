39e196e22832e10bdf7dd92d5f4e8385db78b110
Seth Purr <eric.purrington@gmail.com>
2020-06-02 16:24:37 -0700

    changed connection for heroku

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,18 @@
 var mysql = require("mysql");
 require("dotenv").config();
+var connection;
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: process.env.MYSQL_HOST,
+        port: process.env.MYSQL_PORT,
+        user: process.env.MYSQL_USER,
+        password: process.env.MYSQL_PASSWORD,
+        database: "burgers_db",
+    });
+}
 
-var connection = mysql.createConnection({
-    host: process.env.MYSQL_HOST,
-    port: process.env.MYSQL_PORT,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: "burgers_db",
-});
 
 connection.connect(function (err) {
     if (err) throw err;
